31049b2ad9a6f1447637ea7f6d7d6738992ae95f
herve <crespel.herve@wanadoo.fr>
2025-11-06 15:36:55 +0100

    API

--- Sources/Taxionomy/TaxionomyAPI.swift
+++ Sources/Taxionomy/TaxionomyAPI.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+public struct Manager : View {
+    @State var taxionomy = Taxionomy(taxionomie)
+    @State var selected = 0
+    
+    @State var taxion = Taxion()
+    
+    public init() {}
+    
+    public var body: some View {
+        TabView(selection: $selected) {
+            TaxionomyShow(taxionomy: taxionomy)
+                .tabItem { Text("liste") }.tag(1)
+            TaxionPicker($taxion, $taxionomy, {})
+                .tabItem { Text("picker") }.tag(2)
+        }.padding()
+    }
+}
+
+#Preview {
+    Manager()
+}
