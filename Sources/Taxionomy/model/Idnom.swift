cbb8f54024ef1a07e42a56a07a1adef207e7602d
herve <crespel.herve@wanadoo.fr>
2025-11-26 15:11:37 +0100

    corr

--- Sources/Taxionomy/model/Idnom.swift
+++ Sources/Taxionomy/model/Idnom.swift
@@ -5,6 +5,7 @@
 //  Created by Herve Crespel on 26/11/2025.
 //
 
+// structure de stockage simplifié d'un taxion  
 public struct Idnom: Codable {
     var id:     String   // identifiant d'un taxion dans une taxionomie
     var nom:    String
@@ -13,6 +14,13 @@ public struct Idnom: Codable {
         id = taxion.id
         nom = taxion.short
     }
+    
+    public init() {
+        id = ""
+        nom = ""
+    }
+    
+    public var isNaN : Bool { id == "" && nom == "" }
     
     public func noms(_ taxionomy:Taxionomy) -> [String] {
         taxionomy.find(id).noms
