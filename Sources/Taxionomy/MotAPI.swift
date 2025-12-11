9735c69c4fd99422490d78e545b1543fb6acec19
herve <crespel.herve@wanadoo.fr>
2025-12-11 12:19:09 +0100

    corr

---
+++ Sources/Taxionomy/MotAPI.swift
@@ -0,0 +1,59 @@
+//
+//  MotAPI.swift
+//  Taxionomy
+//
+//  Created by Herve Crespel on 11/12/2025.
+//
+import SwiftUI
+
+
+
+public struct MotAPI: View {
+    @State var mot = Mot("chose", "choses", .f)
+    @State var nompropre = "Boris Goudounov"
+    @State var ligne = "une ligne courte"
+    @State var simple = 0
+    
+    public var body:some View {
+        Form {
+            VStack {
+                TabView(selection: $simple) {
+                    MotView($mot)
+                        .tabItem { Text("mot") }.tag(0)
+                    NomPropre($nompropre, "nom propre", "nom propre" )
+                        .tabItem { Text("nom propre") }.tag(1)
+                    Line(input:$nompropre)
+                        .tabItem { Text("ligne") }.tag(2)
+                }
+                
+                HStack {
+                    Text("Mot").font(.title2)
+                        .frame(width:150)
+                    MotView($mot).frame(width:400)
+                        .padding(10).border(.gray).cornerRadius(3)
+                }.frame(width:600, alignment:.leading)
+                    .padding(10)
+                HStack {
+                    Text("NomPropre").font(.title2)
+                        .frame(width:150)
+                    NomPropre($nompropre, "compositeur", "aide" )
+                        .frame(width:400)
+                        .padding(10).border(.gray).cornerRadius(3)
+                }.frame(width:600, alignment:.leading)
+                    .padding(10)
+                HStack {
+                    Text("Line").font(.title2)
+                        .frame(width:150)
+                    Line(input:$ligne)
+                        .frame(width:400)
+                        .padding(10).border(.gray).cornerRadius(3)
+                }.frame(width:600, alignment:.leading)
+                    .padding(10)
+            }.padding()
+        }
+    }
+}
+
+#Preview {
+    MotAPI()
+}
