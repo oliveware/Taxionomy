7bde59f145b8b3ad7f70d1f9fbb1185cfbb3c5bd
herve <crespel.herve@wanadoo.fr>
2026-02-05 16:07:52 +0100

    pointest

---
+++ Sources/Taxionomy/view/EquipementPicker.swift
@@ -0,0 +1,89 @@
+//
+//  EquipementPicker.swift
+//  Taxionomy
+//
+//  Created by Herve Crespel on 05/02/2026.
+//
+
+import SwiftUI
+
+public struct EquipementPicker : View {
+    var taxionomy : Taxionomy
+    @Binding var equipement: Taxion
+    var done: () -> Void = {}
+    @State var parentid : String
+    var fonction : String
+    
+    public init(_ equipement:Binding<Taxion>, _ taxionomie:Taxionomy = Taxionomy.besoins, _ fonction:String, _ done: @escaping () -> Void) {
+        _equipement = equipement
+        taxionomy = taxionomie
+        pick = equipement.wrappedValue.isNaN
+        self.done = done
+        self.fonction = fonction
+        parentid = fonction
+    }
+        
+    var children : [Taxion] {
+        let taxion = taxionomy.find(parentid)
+        var liste : [Taxion] = []
+        if taxion.dim < taxionomy.levels.count {
+            let children = taxionomy.levels[taxion.dim].children(taxion)
+            liste = taxion.dim > 2  ? children.sorted(by: <) : children
+        }
+        return liste
+    }
+    
+    
+    @State private var pick :Bool
+    public var body: some View {
+        VStack {
+            if pick {
+                if children.count > 0 {
+                    ScrollView {
+                        ForEach(children) { selected in
+                            Button(action:{
+                                if taxionomy.children(selected).count == 0 {
+                                    equipement = selected
+                                    pick = false
+                                } else {
+                                    parentid = selected.id
+                                }
+                            })
+                            {Text(selected.nom).frame(width:100)}
+                        }
+                    }.frame(height:250, alignment:.leading)
+                    //.padding(.leading, CGFloat(taxion.dim * 80))
+                } else {
+                    Text("aucun choix")
+                }
+                
+            } else {
+                HStack{
+                    //  Text(taxion.id)
+                    Text(equipement.nom)
+                    Spacer()
+                    Button(action:{
+                        done()
+                        pick = true
+                        parentid = fonction
+                    })
+                    {Image(systemName: "pencil")}
+                }.padding()
+                //  }.frame(width:600, height:300, alignment:.leading)
+            }
+        }.padding()
+    }
+}
+
+struct EquipementPrepicker : View {
+    @State var taxion = Taxion()
+    
+    var body: some View {
+        EquipementPicker($taxion, Taxionomy.besoins, "2-9-33",  {})
+            .frame(width:300, height:300)
+    }
+}
+
+#Preview {
+    EquipementPrepicker()
+}
